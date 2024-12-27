d4c82439e182080735de35e545cf98b510a02c45
Psiman62 <simon_dudley@msn.com>
2024-12-28 02:12:23 +1000

    DTOMaker updates

--- DTOMakerV10.Models/Models.cs
+++ DTOMakerV10.Models/Models.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace DTOMakerV10.Models
 {
-    [Entity][EntityTag(1)][Layout(LayoutMethod.SequentialV1)] public interface IData_Int16 { [Member(1)] Int16 Value { get; set; } }
-    [Entity][EntityTag(2)][Layout(LayoutMethod.SequentialV1)] public interface IData_Int32 { [Member(1)] Int32 Value { get; set; } }
-    [Entity][EntityTag(3)][Layout(LayoutMethod.SequentialV1)] public interface IData_Int64 { [Member(1)] Int64 Value { get; set; } }
+    [Entity][EntityKey(1)][Layout(LayoutMethod.SequentialV1)] public interface IData_Int16 { [Member(1)] Int16 Value { get; set; } }
+    [Entity][EntityKey(2)][Layout(LayoutMethod.SequentialV1)] public interface IData_Int32 { [Member(1)] Int32 Value { get; set; } }
+    [Entity][EntityKey(3)][Layout(LayoutMethod.SequentialV1)] public interface IData_Int64 { [Member(1)] Int64 Value { get; set; } }
 }
