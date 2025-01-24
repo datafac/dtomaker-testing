298cb97948781b98d03a8cf479abdddb7860af50
Psiman62 <simon_dudley@msn.com>
2025-01-24 18:31:54 +1000

    adding object graph tests

---
+++ DTOMakerV10.Models3/Models.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using DTOMaker.Models;
+using DTOMaker.Models.MemBlocks;
+using DTOMaker.Models.MessagePack;
+namespace DTOMakerV10.Models3
+{
+    [Entity][EntityKey(1)][Layout(LayoutMethod.Linear)]
+    public interface INode
+    {
+        [Member(1)] String Key { get; set; }
+    }
+
+    [Entity][EntityKey(2)][Layout(LayoutMethod.Linear)]
+    public interface IStringNode : INode
+    {
+        [Member(1)] String Value { get; set; }
+    }
+
+    [Entity][EntityKey(3)][Layout(LayoutMethod.Linear)]
+    public interface INumericNode : INode
+    {
+    }
+
+    [Entity][EntityKey(4)][Layout(LayoutMethod.Linear)]
+    public interface IInt64Node : INumericNode
+    {
+        [Member(1)] Int64 Value { get; set; }
+    }
+
+    [Entity][EntityKey(5)][Layout(LayoutMethod.Linear)]
+    public interface IDoubleNode : INumericNode
+    {
+        [Member(1)] Double Value { get; set; }
+    }
+
+    [Entity][EntityKey(6)][Layout(LayoutMethod.Linear)]
+    public interface IBooleanNode : INode
+    {
+        [Member(1)] Boolean Value { get; set; }
+    }
+
+    [Entity][EntityKey(10)][Layout(LayoutMethod.Linear)]
+    public interface ITree
+    {
+        [Member(1)] ITree? Left { get; set; }
+        [Member(2)] ITree? Right { get; set; }
+        [Member(3)] INode? Node { get; set; }
+        [Member(4)] int Size { get; set; }
+    }
+}
---
+++ DTOMakerV10.Tests/ObjectGraphTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using MessagePack;
+using System;
+using Xunit;
+
+namespace DTOMakerV10.Tests
+{
+    public class ObjectGraphTests
+    {
+        [Fact]
+        public void RoundTripViaMessagePack()
+        {
+            var orig = new DTOMakerV10.Models3.CSPoco.Tree()
+            {
+                Left = new Models3.CSPoco.Tree()
+                {
+                    Node = new Models3.CSPoco.DoubleNode() { Key = "Double", Value = 123.456D },
+                    Size = 1,
+                },
+                Right = new Models3.CSPoco.Tree()
+                {
+                    Node = new Models3.CSPoco.BooleanNode() { Key = "Boolean", Value = true },
+                    Size = 1,
+                },
+                Node = new Models3.CSPoco.StringNode() { Key = "String", Value = "abcdef" },
+                Size = 3,
+            };
+            orig.Freeze();
+
+            DTOMakerV10.Models3.MessagePack.Tree sender = DTOMakerV10.Models3.MessagePack.Tree.CreateFrom(orig);
+            sender.Freeze();
+
+            ReadOnlyMemory<byte> buffer = MessagePackSerializer.Serialize<DTOMakerV10.Models3.MessagePack.Tree>(sender);
+            DTOMakerV10.Models3.MessagePack.Tree recver = MessagePackSerializer.Deserialize<DTOMakerV10.Models3.MessagePack.Tree>(buffer);
+            recver.Freeze();
+
+            var copy = DTOMakerV10.Models3.CSPoco.Tree.CreateFrom(recver);
+            copy.Freeze();
+            copy.Should().NotBeNull();
+            copy.Should().Be(orig);
+            copy.Equals(orig).Should().BeTrue();
+        }
+    }
+}
