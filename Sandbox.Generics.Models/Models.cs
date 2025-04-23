c71a0f23c13c92dd5d36ab0713682eb756f86791
Psiman62 <simon_dudley@msn.com>
2025-04-23 20:02:09 +1000

    generics - messagepack

---
+++ Sandbox.Generics.Models/Models.cs
@@ -0,0 +1,22 @@
+﻿using DTOMaker.Models;
+
+namespace Sandbox.Generics.Models
+{
+    [Entity]
+    [Id(100)]
+    public interface ITree<TKey, TValue>
+    {
+        [Member(1)] int Count { get; }
+        [Member(2)] TKey Key { get; }
+        [Member(3)] TValue Value { get; }
+        [Member(4)] ITree<TKey, TValue>? Left { get; }
+        [Member(5)] ITree<TKey, TValue>? Right { get; }
+    }
+
+    [Entity]
+    [Id(1)]
+    public interface IMyTree : ITree<string, long>
+    {
+
+    }
+}
---
+++ Sandbox.Generics.Tests/GenericsTests.cs
@@ -0,0 +1,37 @@
+using MessagePack;
+using Sandbox.Generics.Models.MessagePack;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace Sandbox.Generics.Tests
+{
+    public class GenericsTests
+    {
+        [Fact]
+        public void Roundtrip_MessagePack()
+        {
+            var sendMsg = new MyTree()
+            {
+                Count=1,
+                Key = "abc",
+                Value = 456L,
+                Left = null,
+                Right = null,
+            };
+            sendMsg.Freeze();
+
+            // act
+            ReadOnlyMemory<byte> buffer = MessagePackSerializer.Serialize<MyTree>(sendMsg);
+            MyTree recdMsg = MessagePackSerializer.Deserialize<MyTree>(buffer);
+            recdMsg.Freeze();
+
+            // assert
+            // - equality
+            recdMsg.ShouldNotBeNull();
+            recdMsg!.Equals(sendMsg).ShouldBeTrue();
+            recdMsg.ShouldBe(sendMsg);
+            recdMsg.GetHashCode().ShouldBe(sendMsg.GetHashCode());
+        }
+    }
+}
