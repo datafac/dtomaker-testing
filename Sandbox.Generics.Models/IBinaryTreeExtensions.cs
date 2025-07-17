a125fd92c4e4ee05c353eaed7169e12252d422e9
Psiman <simon@appsbyday.com>
2025-07-17 17:09:03 +1000

    more binary tree tests

--- Sandbox.Generics.Models/IBinaryTreeExtensions.cs
+++ Sandbox.Generics.Models/IBinaryTreeExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using DTOMaker.Runtime;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 
@@ -61,6 +60,13 @@ namespace Sandbox.Generics.Models
             tree.Depth = newDepth;
         }
 
+        private static TEntity Unfrozen<TEntity>(TEntity entity) where TEntity : class
+        {
+            return entity is IFreezable freezable && freezable.IsFrozen
+                ? freezable.PartCopy() as TEntity ?? throw new InvalidOperationException("Failed to create unfrozen copy.")
+                : entity;
+        }
+
         public static IBinaryTree<TKey, TValue> AddOrUpdate<TKey, TValue>(this IBinaryTree<TKey, TValue> tree, TKey key, TValue value,
             IBinaryTreeFactory<TKey, TValue> nodeFactory, bool allowDuplicates = false)
             where TKey : notnull, IComparable<TKey>
@@ -108,7 +114,7 @@ namespace Sandbox.Generics.Models
             }
 
             // rebalance if needed
-            bool rebalanced = false;
+            bool rotated = false;
             // doco: https://en.wikipedia.org/wiki/Tree_rotation
             if (result.Left is not null && (result.Left.Depth - (result.Right?.Depth ?? 0)) > 1)
             {
@@ -116,43 +122,41 @@ namespace Sandbox.Generics.Models
                 // todo special case
                 var newRoot = result.Left;
                 result.Left = newRoot.Right;
+                // unfreeze newRoot if needed
+                newRoot = Unfrozen(newRoot);
                 newRoot.Right = result;
                 result = newRoot;
-                rebalanced = true;
+                rotated = true;
             }
             else if (result.Right is not null && (result.Right.Depth - (result.Left?.Depth ?? 0)) > 1)
             {
                 // right-heavy, perform left rotation
                 var newRoot = result.Right;
                 result.Right = newRoot.Left;
+                // unfreeze newRoot if needed
+                newRoot = Unfrozen(newRoot);
                 newRoot.Left = result;
                 result = newRoot;
-                rebalanced = true;
+                rotated = true;
             }
 
-            if (rebalanced)
+            if (rotated)
             {
                 // recalc count/depth for children
                 if (result.Left is not null)
                 {
                     result.Left.TrySetCount();
                     result.Left.TrySetDepth();
-                    //result.Left.Count = 1 + (result.Left.Left?.Count ?? 0) + (result.Left.Right?.Count ?? 0);
-                    //result.Left.Depth = 1 + Math.Max(result.Left.Left?.Depth ?? 0, result.Left.Right?.Depth ?? 0);
                 }
                 if (result.Right is not null)
                 {
                     result.Right.TrySetCount();
                     result.Right.TrySetDepth();
-                    //result.Right.Count = 1 + (result.Right.Left?.Count ?? 0) + (result.Right.Right?.Count ?? 0);
-                    //result.Right.Depth = 1 + Math.Max(result.Right.Left?.Depth ?? 0, result.Right.Right?.Depth ?? 0);
                 }
             }
 
             result.TrySetCount();
             result.TrySetDepth();
-            //result.Count = 1 + (result.Left?.Count ?? 0) + (result.Right?.Count ?? 0);
-            //result.Depth = 1 + Math.Max(result.Left?.Depth ?? 0, result.Right?.Depth ?? 0);
             return result;
         }
     }
--- Sandbox.Generics.Tests/GenericsTests_Trees.cs
+++ Sandbox.Generics.Tests/GenericsTests_Trees.cs
@@ -1,9 +1,9 @@
 using DataFac.Storage;
 using DTOMaker.Runtime;
-using Newtonsoft.Json.Linq;
 using Sandbox.Generics.Models;
 using Shouldly;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -118,7 +118,7 @@ namespace Sandbox.Generics.Tests
         [InlineData(ImplKind.JsonNewtonSoft, "abcdefg", 3)]
         [InlineData(ImplKind.MessagePack, "abcdefg", 3)]
         [InlineData(ImplKind.MemBlocks, "abcdefg", 3)]
-        public void AddValue(ImplKind impl, string order, int expectedDepth)
+        public void AddValue(ImplKind impl, string order, int maxDepth)
         {
             using var dataStore = new DataFac.Storage.Testing.TestDataStore();
 
@@ -155,7 +155,7 @@ namespace Sandbox.Generics.Tests
                 }
             }
             tree.Count.ShouldBe(count);
-            tree.Depth.ShouldBe(expectedDepth);
+            tree.Depth.ShouldBeLessThanOrEqualTo(maxDepth);
 
             var node = tree.Get("b");
             node.ShouldNotBeNull();
@@ -166,5 +166,85 @@ namespace Sandbox.Generics.Tests
             node.ShouldBeNull();
         }
 
+        private IEnumerable<string> GetCharCombinations(string chars)
+        {
+            if (chars.Length == 1)
+            {
+                yield return new string(chars[0], 1);
+            }
+            else
+            {
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    char ch = chars[i];
+                    string remaining = chars.ToString().Remove(i, 1);
+                    foreach (var subCombination in GetCharCombinations(remaining))
+                    {
+                        yield return ch + subCombination;
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData("ab", "ab,ba")]
+        [InlineData("abc", "abc,acb,bac,bca,cab,cba")]
+        [InlineData("abcd", "abcd,abdc,acbd,acdb,adbc,adcb,bacd,badc,bcad,bcda,bdac,bdca,cabd,cadb,cbad,cbda,cdab,cdba,dabc,dacb,dbac,dbca,dcab,dcba")]
+        public void CheckCharCombinations(string input, string expected)
+        {
+            string[] combinations = GetCharCombinations(input).ToArray();
+            string.Join(",", combinations).ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData("a", 1)]
+        [InlineData("ab", 2)]
+        [InlineData("abc", 3)]
+        [InlineData("abcd", 3)]
+        [InlineData("abcde", 4)]
+        [InlineData("abcdef", 4)]
+        [InlineData("abcdefg", 4)]
+        public void AllCombinations(string chars, int maxDepth)
+        {
+            var nodeFactory = GetNodeFactory(ImplKind.CSPoco);
+            using var dataStore = new DataFac.Storage.Testing.TestDataStore();
+
+            foreach (string order in GetCharCombinations(chars))
+            {
+                var tree = nodeFactory.CreateEmpty();
+                {
+                    if (tree is IPackable packable) packable.Pack(dataStore);
+                    if (tree is IFreezable freezable) freezable.Freeze();
+                }
+                tree.Count.ShouldBe(0);
+                tree.Depth.ShouldBe(0);
+
+                // add nodes in order
+                int count = 0;
+                foreach (char ch in order)
+                {
+                    long value = (Char.IsLetter(ch) && Char.IsLower(ch)) ? (ch - 'a') + 1 : throw new ArgumentException($"Unexpected character: {ch}");
+                    tree = tree.AddOrUpdate(new string(ch, 1), value, nodeFactory);
+                    count++;
+
+                    // pack and freeze the tree after each addition
+                    if (tree is IPackable packable) packable.Pack(dataStore);
+                    if (tree is IFreezable freezable) freezable.Freeze();
+                }
+
+
+                var pairs = tree.GetKeyValuePairs().ToArray();
+                for (int i = 0; i < pairs.Length; i++)
+                {
+                    if (i > 0)
+                    {
+                        pairs[i].Key.ShouldBeGreaterThan(pairs[i - 1].Key);
+                    }
+                }
+                tree.Count.ShouldBe(count);
+                int depth = tree.Depth;
+                depth.ShouldBeLessThanOrEqualTo(maxDepth, $"Input='{order}',depth={depth}");
+            }
+        }
     }
 }
