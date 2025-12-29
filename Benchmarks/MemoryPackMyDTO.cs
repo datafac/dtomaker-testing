83e14b5c5182154d6239693cb1e164b122f77383
Psiman62 <psiman-hju@bytz.me>
2025-12-29 21:46:35 +1000

    MsgPack2 tests

--- Benchmarks/MemoryPackMyDTO.cs
+++ Benchmarks/MemoryPackMyDTO.cs
@@ -22,7 +22,7 @@ namespace TestModels.MemPack
         public override int GetHashCode()
         {
             HashCode hasher = new();
-            hasher.Add(this.GetType());
+            hasher.Add(GetType());
             return hasher.ToHashCode();
         }
         #endregion
@@ -35,6 +35,7 @@ namespace TestModels.MemPack
         #region IEquatable implementation
         public bool Equals(Quadrilateral? other) => other is not null && base.Equals(other);
         public override bool Equals(object? obj) => obj is Quadrilateral other && Equals(other);
+        public override int GetHashCode() => base.GetHashCode();
         #endregion
     }
 
@@ -49,8 +50,8 @@ namespace TestModels.MemPack
         {
             return other is not null
                 && base.Equals(other)
-                && this.Length == other.Length
-                && this.Height == other.Height;
+                && Length == other.Length
+                && Height == other.Height;
         }
 
         public override bool Equals(object? obj) => obj is Rectangle other && Equals(other);
@@ -74,15 +75,15 @@ namespace TestModels.MemPack
         {
             return new MemoryPackMyDTO
             {
-                Field01 = this.Field01,
-                Field02LE = this.Field02LE,
-                Field03BE = this.Field03BE,
-                Field04 = this.Field04,
-                Field05 = this.Field05,
-                Field06 = this.Field06,
-                Field07 = this.Field07,
-                Field08 = this.Field08,
-                Field09 = this.Field09
+                Field01 = Field01,
+                Field02LE = Field02LE,
+                Field03BE = Field03BE,
+                Field04 = Field04,
+                Field05 = Field05,
+                Field06 = Field06,
+                Field07 = Field07,
+                Field08 = Field08,
+                Field09 = Field09
             };
         }
 
@@ -91,39 +92,52 @@ namespace TestModels.MemPack
         [MemoryPackInclude] public double Field03BE { get; set; }
         [MemoryPackInclude] public Guid Field04 { get; set; }
         [MemoryPackInclude] public string? Field05 { get; set; }
-        [MemoryPackInclude] public ReadOnlyMemory<byte> Field06 { get; set; }
+        [MemoryPackInclude] public ReadOnlyMemory<byte>? Field06 { get; set; }
         [MemoryPackInclude] public PairOfInt16 Field07 { get; set; }
         [MemoryPackInclude] public PairOfInt32 Field08 { get; set; }
         [MemoryPackInclude] public PairOfInt64 Field09 { get; set; }
-        [MemoryPackIgnore] Octets? IMyDTO.Field06 { get => Octets.UnsafeWrap(this.Field06); set => this.Field06 = (value is null) ? default : value.AsMemory(); }
+        [MemoryPackIgnore]
+        Octets? IMyDTO.Field06
+        {
+            get => Field06 is null ? null : Octets.UnsafeWrap(Field06.Value);
+            set => Field06 = (value is null) ? null : value.AsMemory();
+        }
 
         #region IEquatable implementation
+        private static bool MemoryEqual(ReadOnlyMemory<byte>? a, ReadOnlyMemory<byte>? b)
+        {
+            return a is null ? b is null : b is not null && a.Value.Span.SequenceEqual(b.Value.Span);
+        }
+
         public bool Equals(MemoryPackMyDTO? other)
         {
             return other is not null
-                && this.Field01 == other.Field01
-                && this.Field02LE == other.Field02LE
-                && this.Field03BE == other.Field03BE
-                && this.Field04 == other.Field04
-                && this.Field05 == other.Field05
-                && this.Field06.Span.SequenceEqual(other.Field06.Span)
-                && this.Field07.Equals(other.Field07)
-                && this.Field08.Equals(other.Field08)
-                && this.Field09.Equals(other.Field09);
+                && Field01 == other.Field01
+                && Field02LE == other.Field02LE
+                && Field03BE == other.Field03BE
+                && Field04 == other.Field04
+                && Field05 == other.Field05
+                && MemoryEqual(Field06, other.Field06)
+                && Field07.Equals(other.Field07)
+                && Field08.Equals(other.Field08)
+                && Field09.Equals(other.Field09);
         }
 
         public override bool Equals(object? obj) => obj is MemoryPackMyDTO other && Equals(other);
         public override int GetHashCode()
         {
             HashCode hasher = new();
-            hasher.Add(this.GetType());
+            hasher.Add(GetType());
             hasher.Add(Field01);
             hasher.Add(Field02LE);
             hasher.Add(Field03BE);
             hasher.Add(Field04);
             hasher.Add(Field05);
-            hasher.Add(Field06.Length);
-            hasher.AddBytes(Field06.Span);
+            if (Field06 is not null)
+            {
+                hasher.Add(Field06.Value.Length);
+                hasher.AddBytes(Field06.Value.Span);
+            }
             hasher.Add(Field07);
             hasher.Add(Field08);
             hasher.Add(Field09);
