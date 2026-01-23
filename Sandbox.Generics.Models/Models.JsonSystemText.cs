3f95691fbf11ffe844aa6b1843b463481381bd66
Psiman62 <psiman-hju@bytz.me>
2026-01-23 22:18:30 +1000

    dtomaker updates

--- Sandbox.Generics.Models/Models.JsonSystemText.cs
+++ Sandbox.Generics.Models/Models.JsonSystemText.cs
@@ -2,17 +2,4 @@
 
 namespace Sandbox.Generics.Models.JsonSystemText
 {
-    public partial class MyBinaryTree : IBinaryTree<string, long>
-    {
-        IBinaryTree<string, long>? IBinaryTree<string, long>.Left
-        {
-            get => Left;
-            set => Left = value is null ? null : new MyBinaryTree((IMyBinaryTree)value);
-        }
-        IBinaryTree<string, long>? IBinaryTree<string, long>.Right
-        {
-            get => Right;
-            set => Right = value is null ? null : new MyBinaryTree((IMyBinaryTree)value);
-        }
-    }
 }
--- Sandbox.Generics.Models/Models.cs
+++ Sandbox.Generics.Models/Models.cs
@@ -2,17 +2,6 @@
 
 namespace Sandbox.Generics.Models
 {
-    [Entity(1, LayoutMethod.Linear)]
-    public interface IMyBinaryTree : IEntityBase
-    {
-        [Member(1)] int Count { get; set; }
-        [Member(2)] byte Depth { get; set; }
-        [Member(3)] string Key { get; set; }
-        [Member(4)] long Value { get; set; }
-        [Member(5)] IMyBinaryTree? Left { get; set; }
-        [Member(6)] IMyBinaryTree? Right { get; set; }
-    }
-
     //[Entity]
     //[Id(2)]
     //[Layout(LayoutMethod.Linear)]
